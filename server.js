956a9352245fa287ac9fd3ee7a4fa0b68bd4fcf7
John Kennedy <devjckennedy@gmail.com>
2018-05-09 19:44:49 +0100

    Update server.js with latest changes

--- server.js
+++ server.js
@@ -4,22 +4,21 @@ const mongoose = require('mongoose');
 
 const MovieController = require('./controllers/movieController');
 
-const app = express();
-app.use(express.static('public'));
-
-const { PORT, DATABASE_URL } = require('./config');
-const { Movie } = require('./models/movie');
-
 mongoose.Promise = global.Promise;
 
+const app = express();
+const { PORT, DATABASE_URL } = require('./config');
 
-app.listen(process.env.PORT || 8080);
+mongoose.connect(DATABASE_URL)
+        .then(() => console.log('Connected to database'))
+        .catch(() => console.log('Failed to connect to database'));
 
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
 	extended: true
 }));
+app.use(bodyParser.json());
 
+app.use(express.static('public'));
 
 app.get('/', function(req, res){
 	res.send('hello');
@@ -30,5 +29,7 @@ app.get('/movies', MovieController.getMovies);
 app.post('/movies', MovieController.addMovie);
 
 app.delete('/movies/:title', MovieController.deleteMovie);
+
+app.listen(process.env.PORT || 8080);
 
 module.exports = app;
